7cf5b7fc724c956b0a0767dd7c6dab152dc2cb18
maz <mz@e7chong.com>
2016-02-19 13:35:11 +0800

    细节优化

--- static/js/blog_replay.js
+++ static/js/blog_replay.js
@@ -33,3 +33,7 @@ function a(id) {
     });
     }
 
+$(document).ready(function(){
+    var host = window.location.host;
+    document.getElementById('blog_search').href = "http://" + host + "/search_blog";
+});

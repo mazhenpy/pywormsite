2a785a2be7ebeea3900248c407b6e1a9ebf53e20
pyworm <1053556056@qq.com>
2016-12-26 11:31:45 +0800

    /

--- handlers/weixin/weixin_translate.py
+++ handlers/weixin/weixin_translate.py
@@ -135,7 +135,7 @@ class WeixintranslateHandler(tornado.web.RequestHandler):
         if content == '杀的你喊妈':
             # demo = Daiwan()
             # word = demo.get_user_info(content)
-            word = 'hehe'
+            word = content
             from PIL import Image, ImageDraw, ImageFont
 
             font = ImageFont.truetype('simsun.ttc', 24)
@@ -146,7 +146,7 @@ class WeixintranslateHandler(tornado.web.RequestHandler):
             # draw.text((0, 60), unicode('你好', 'utf-8'), (0, 0, 0), font=font)
             img.save('/root/mazhen/pyworm-blog/pywormsite/static/lol/' + 'lol1.png')
 
-            picurl = 'http://139.196.43.6/static/lol/lol1.jpg'
+            picurl = 'http://139.196.43.6/static/lol/lol1.png'
 
             data = '''
                 <xml>

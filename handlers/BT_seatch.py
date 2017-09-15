91a7a7afc59ebe78c20bec80ff0c99317aec47b8
pyworm <1053556056@qq.com>
2017-09-15 14:02:59 +0800

    1

--- handlers/BT_seatch.py
+++ handlers/BT_seatch.py
@@ -47,8 +47,6 @@ class BtSearchHandler(tornado.web.RequestHandler):
             mongo_find = db.bt_info.find({'$or': [{'name': {'$regex': bt_keywords, '$options': 'i'}},
                                                   {"files": {"$elemMatch": {"file_name": {'$regex': bt_keywords}}}}]})
 
-            mongo_find_clone = mongo_find.clone()
-
             # 如果相关数据超过100条，只按100条算
             max_data = mongo_find.skip(10 * (int(page_index) + 9)).limit(1)
             # # max_data = mongo_find.skip(10).limit(1)
@@ -74,7 +72,8 @@ class BtSearchHandler(tornado.web.RequestHandler):
                 print(222222222222)
                 bt_count = int(mongo_find.count())
 
-            links = mongo_find_clone.sort(
+            links = db.bt_info.find({'$or': [{'name': {'$regex': bt_keywords, '$options': 'i'}},
+                                             {"files": {"$elemMatch": {"file_name": {'$regex': bt_keywords}}}}]}).sort(
                 'create_at', pymongo.ASCENDING).skip(10 * (int(page_index) - 1)).limit(10)
 
             page_num = int(bt_count / 10) + 1  # 共有几页
